62aeedbc6c51ad47f78387569806f07b1eb2885e
lunahc92 <lunahc92@gmail.com>
2020-03-10 15:17:30 +0900

    Add Engine Auto Deleter Component

---
+++ devspace/src/components/EngineAutoDelete.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <v-switch v-model="autoDelete"
+                  label="자동삭제"
+        >
+        </v-switch>
+        <v-btn @click="showState()">show state</v-btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DevSpaceAutoDeleter',
+        props: ['autoDelete'],
+        methods: {
+            showState() {
+                alert(this.autoDelete);
+            }
+        }
+    }
+</script>
+
+<style>
+    /* 대충 공대생 디자인... */
+</style>
